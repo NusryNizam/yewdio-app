60dbd9af0d08e2495b4b8fd2d5f30f18e98955a6
Nusry Nizam <nusrynizam@gmail.com>
2024-02-18 12:59:13 +0530

    improvement: added next insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
 import { inter } from "@/utils/fonts";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
@@ -13,9 +14,8 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: 'Yewdio',
-  description:
-    'Listen to YouTube as audio'
+  title: "Yewdio",
+  description: "Listen to YouTube as audio",
 };
 
 export default function RootLayout({
@@ -24,11 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
+    <html lang="en">
       <Head>
         <meta name="theme-color" content="#151515" />
-        <meta name="description" content="Listen to YouTube as audio on-the-go." />
-        <link rel="manifest" href="./manifest.webmanifest" />
+        <meta
+          name="description"
+          content="Listen to YouTube as audio on-the-go."
+        />
+        <link
+          rel="manifest"
+          href="./manifest.webmanifest"
+        />
       </Head>
       <body className={`${inter.className}`}>
         <main className="main">
@@ -48,6 +54,7 @@ export default function RootLayout({
               },
             }}
           />
+          <SpeedInsights />
         </main>
       </body>
     </html>
